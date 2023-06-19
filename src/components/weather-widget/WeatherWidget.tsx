763de61463c1e6473df50a9ff7332f44caf61988
vegardandersson <veg_ander@hotmail.com>
2023-06-19 15:15:11 +0200

    added searchbar, added cities.json

--- src/components/weather-widget/WeatherWidget.tsx
+++ src/components/weather-widget/WeatherWidget.tsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather-widget.css";
+import axios from "axios";
+import cities from 'cities.json';
 
 function WeatherWidget() {
+
+    const [searching, setSearching] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    function searchCities() {
+        /*const options = {
+            method: 'GET',
+            url: 'https://andruxnet-world-cities-v1.p.rapidapi.com/',
+            params: {
+              query: 'par',
+              searchby: 'city'
+            },
+            headers: {
+              'X-RapidAPI-Key': 'e4905e40eemsh7be5068681dc45bp1a35cajsn7e67c1df32c6',
+              'X-RapidAPI-Host': 'andruxnet-world-cities-v1.p.rapidapi.com'
+            }
+          };
+        try {
+            const response = await axios.request(options);
+            console.log(response);
+        } catch (e) {
+            console.error(e); 
+        }*/
+
+        //@ts-ignore
+        console.log(cities.filter(e => e.name === "Paris"))
+    }
+
     return (
         <div className="weather-widget">
-            <div className="header">
-                <h1 className="city-name">Trondheim</h1>
-                <img className="flag-icon" src="https://flagsapi.com/NO/flat/64.png"></img>
-            </div>
+            {searching ? 
+                <input className="search-bar" placeholder="Find city..."></input>
+                       :
+                <div className="header" onClick={() => searchCities()}>
+                    <h1 className="city-name">Trondheim</h1>
+                    <img className="flag-icon" src="https://flagsapi.com/NO/flat/64.png"></img>
+                </div>
+                       }
             <img className="weather-icon" src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"></img>
             <h1 className="temperature">21°C</h1>
             <h2 className="time-of-day">12:44</h2>
